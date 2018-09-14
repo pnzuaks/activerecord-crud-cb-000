ff8175d0e29bf58934b6a30f8cdb668c0658f8a0
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 19:20:12 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -47,7 +47,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  
+
   Movie.select(:id).first
 end
 
@@ -64,7 +64,7 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  Movie.where("release_date > 2002", p[:release_date]).order("release_date DESC")
+  Movie.where("release_date > 2002", 2002).order("release_date DESC")
 end
 
 def can_be_found_updated_and_saved
