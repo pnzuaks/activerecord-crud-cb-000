e14cbbfd8f9dd1de1ec928589bd21ffb3bf3a6f8
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 17:52:39 +0000

    automatically backed up by learn

--- db/migrate/20180914174841_create_movies.rb
+++ db/migrate/20180914174841_create_movies.rb
@@ -5,6 +5,6 @@ class CreateMovies < ActiveRecord::Migration[5.2]
       t.integer :release_date
       t.string :director
       t.string :lead
-      t.boolean :in_theatersrake db:migrate
+      t.boolean :in_theaters
   end
 end
