f0ce083ae9d6f3711498be4024a31da576eec8e7
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 17:51:44 +0000

    automatically backed up by learn

--- db/migrate/20180914174841_create_movies.rb
+++ db/migrate/20180914174841_create_movies.rb
@@ -3,7 +3,7 @@ class CreateMovies < ActiveRecord::Migration[5.2]
     create_table :movies do |t|
       t.string :title
       t.integer :release_date
-      t.integer :age
+      t.string :director
       t.string :hometown
   end
 end
