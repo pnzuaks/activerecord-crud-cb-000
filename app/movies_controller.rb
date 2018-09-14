8a7ab921a3255ded24dfd6d50c5c094b29992d52
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 18:45:51 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -54,8 +54,8 @@ def can_find_by_multiple_attributes
   # Search Values:
   #
   args = {:title == "Title",
-  release_date == 2000
-  director == "Me"}
+  :release_date == 2000,
+  :director == "Me"}
   Movie.find_by(args)
 end
 
