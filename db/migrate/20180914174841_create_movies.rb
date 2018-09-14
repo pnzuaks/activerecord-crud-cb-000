4ae758a9dbf57cee432cd4fa4f60a3f7fb61be3b
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 17:50:17 +0000

    automatically backed up by learn

--- db/migrate/20180914174841_create_movies.rb
+++ db/migrate/20180914174841_create_movies.rb
@@ -1,7 +1,7 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :artists do |t|
-      t.string :name
+      t.string :title
       t.string :genre
       t.integer :age
       t.string :hometown
