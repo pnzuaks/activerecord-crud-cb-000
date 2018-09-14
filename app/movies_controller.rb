826161985d062f90fad162c7bfc04e8f884cf315
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-14 18:40:07 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -47,7 +47,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.select(:id).first
+  Movie.select(:id).first.title
 end
 
 def can_find_by_multiple_attributes
